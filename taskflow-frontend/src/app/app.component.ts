d0e92f38090ab3ee94632fdeaee91bfbe958ec20
Ciro Aiello <ciroaiello@live.com>
2025-02-12 23:14:29 +0100

    Aggiornato codice generato per development 3

--- taskflow-frontend/src/app/app.component.ts
+++ taskflow-frontend/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layout/header/header.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, SidebarComponent]
 })
 export class AppComponent {
   title = 'taskflow-frontend';
