947560f1f29b4407d72f1b21f983c96f2ff0c8b9
Ciro Aiello <ciroaiello@live.com>
2025-02-13 11:02:58 +0100

    Aggiunto codice generato per development 3

---
+++ taskflow-frontend/src/app/pages/projects/projects.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projects',
+  template: '<h2>Progetti</h2>',
+  standalone: true
+})
+export class ProjectsComponent {
+}
