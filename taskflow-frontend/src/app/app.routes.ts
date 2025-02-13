f45ad5a116a53ec51c5bb97833c0f54ec6836d32
Ciro Aiello <ciroaiello@live.com>
2025-02-13 14:55:09 +0100

    Aggiornato codice generato per development 3

--- taskflow-frontend/src/app/app.routes.ts
+++ taskflow-frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { TasksComponent } from './pages/tasks/tasks.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'tasks', component: TasksComponent },
+  { path: '**', redirectTo: '' }
+];
